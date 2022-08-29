bad68cba2d0b10a09fc50c5171c419494e134de4
Zhijian Mei <545902915@qq.com>
2022-08-29 21:32:54 +0800

    Update V_FindTemplateGroups.py

--- V_FindTemplateGroups.py
+++ V_FindTemplateGroups.py
@@ -119,7 +119,7 @@ if __name__ == '__main__':
             filename = root + file
             data = pd.read_csv(filename, delimiter='\t')
 
-            unused_reads_temp = data[data['Score'] >= 0.1]
+            unused_reads_temp = data[data['Score'] >= 0.5]
             unused_reads_temp = unused_reads_temp[-50 < unused_reads_temp['PPM Difference']]
             unused_reads_temp = unused_reads_temp[unused_reads_temp['PPM Difference'] < 50]
             unused_reads_temp.reset_index(inplace=True, drop=True)
@@ -426,38 +426,38 @@ if __name__ == '__main__':
 
         merged_result = result_sequences
 
-        best_result = merged_result[0]
-        best_result_coverage_list = []
-        is_continue = False
-        for best_result_position in range(len(best_result)):
-            current = best_result[best_result_position]
-            if not is_continue and current != ' ':
-                start = best_result_position
-                is_continue = True
-            elif is_continue and current == ' ':
-                end = best_result_position-1
-                is_continue = False
-                best_result_coverage_list.append([start,end])
-            elif is_continue and len(best_result) == best_result_position + 1:
-                best_result_coverage_list.append([start,best_result_position])
-        print(best_result_coverage_list)
-        for ann_key in annotation.keys():
-            if template_id in ann_key:
-                break
-        print(template.id)
-        print(len(template.sequence),len(best_result))
-        print(ann_key,annotation[ann_key])
-        for key in annotation[ann_key].keys():
-            interval = annotation[ann_key][key]
-            print(key,end=',')
-            for i in range(interval[0],interval[1]+1):
-                print(template.sequence[i],end='')
-            print()
-        for contig_array in template.contigArrays:
-            for contig in contig_array:
-                print(contig.template_interval,end='')
-            print()
-        quit()
+        # best_result = merged_result[0]
+        # best_result_coverage_list = []
+        # is_continue = False
+        # for best_result_position in range(len(best_result)):
+        #     current = best_result[best_result_position]
+        #     if not is_continue and current != ' ':
+        #         start = best_result_position
+        #         is_continue = True
+        #     elif is_continue and current == ' ':
+        #         end = best_result_position-1
+        #         is_continue = False
+        #         best_result_coverage_list.append([start,end])
+        #     elif is_continue and len(best_result) == best_result_position + 1:
+        #         best_result_coverage_list.append([start,best_result_position])
+        # print(best_result_coverage_list)
+        # for ann_key in annotation.keys():
+        #     if template_id in ann_key:
+        #         break
+        # print(template.id)
+        # print(len(template.sequence),len(best_result))
+        # print(ann_key,annotation[ann_key])
+        # for key in annotation[ann_key].keys():
+        #     interval = annotation[ann_key][key]
+        #     print(key,end=',')
+        #     for i in range(interval[0],interval[1]+1):
+        #         print(template.sequence[i],end='')
+        #     print()
+        # for contig_array in template.contigArrays:
+        #     for contig in contig_array:
+        #         print(contig.template_interval,end='')
+        #     print()
+        # quit()
 
         step = 250
         html += '*' * 100 + 'Merged Result' + '*' * 100 + '<br>'

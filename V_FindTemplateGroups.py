55455a98cd9b17e9ec58a58018be8d04a65895f8
Zhijian Mei <545902915@qq.com>
2022-08-30 23:31:13 +0800

    Update V_FindTemplateGroups.py

--- V_FindTemplateGroups.py
+++ V_FindTemplateGroups.py
@@ -458,7 +458,7 @@ if __name__ == '__main__':
             tail_query = f'{froot}/tail.fasta'
             os.system(f'prerapsearch -d {contig_filepath} -n {froot}/contigs')
             os.system(f'rapsearch -q {head_query} -d {froot}/contigs -o {froot}/{froot}_head_best_contigs')
-            os.system(f'rapsearch -q {head_query} -d {froot}/contigs -o {froot}/{froot}_tail_best_contigs')
+            os.system(f'rapsearch -q {tail_query} -d {froot}/contigs -o {froot}/{froot}_tail_best_contigs')
             os.system(f'python processRapsearchM8.py -input {froot}/{froot}_head_best_contigs.m8 -output {head_out}')
             os.system(f'python processRapsearchM8.py -input {froot}/{froot}_tail_best_contigs.m8 -output {tail_out}')
             head_df = pd.read_csv(head_out, delimiter='\t', header=None)

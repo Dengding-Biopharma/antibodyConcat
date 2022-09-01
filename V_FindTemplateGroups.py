1b4762872240bae609e0ecbefaa6146f5773ef8f
Zhijian Mei <545902915@qq.com>
2022-09-01 17:10:26 +0800

    Update V_FindTemplateGroups.py

--- V_FindTemplateGroups.py
+++ V_FindTemplateGroups.py
@@ -614,7 +614,7 @@ if __name__ == '__main__':
             # html += '<pre>' + ''.join(read_sequence) + '</pre>'
 
     valueable_contigs = list(Counter(valueable_contigs).keys())
-    graph = naive_db.construct_naive_debruijn_graph(valueable_contigs,3)
+    graph = naive_db.construct_naive_debruijn_graph(valueable_contigs,4)
     outputs = naive_db.output_contigs(graph,[],[])
     outputs = sorted(outputs,key=lambda x:findSupportReadScore(x,sequences_scores),reverse=True)
     for output in outputs:

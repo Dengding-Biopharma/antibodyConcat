de3c18bcae8c25c334b72fc426ce112cc114efdf
Zhijian Mei <545902915@qq.com>
2022-09-05 12:39:56 +0800

    Update V_FindTemplateGroups.py

--- V_FindTemplateGroups.py
+++ V_FindTemplateGroups.py
@@ -492,13 +492,9 @@ if __name__ == '__main__':
                 if best_head_contig not in best_contigs:
                     best_contigs.append(best_head_contig)
                 if best_head_contig not in template.best_fragments:
-                    for item in template.best_fragments:
-                        if best_head_contig not in item:
-                            template.best_fragments.append(best_head_contig)
+                    template.best_fragments.append(best_head_contig)
                 if fragment not in template.best_fragments:
-                    for item in template.best_fragments:
-                        if fragment not in item:
-                            template.best_fragments.append(fragment)
+                    template.best_fragments.append(fragment)
                 # else:
                 #     template.best_fragments.append(fragment)
             except Exception as e:
@@ -522,13 +518,9 @@ if __name__ == '__main__':
                 if best_tail_contig not in best_contigs:
                     best_contigs.append(best_tail_contig)
                 if fragment not in template.best_fragments:
-                    for item in template.best_fragments:
-                        if fragment not in item:
-                            template.best_fragments.append(fragment)
+                    template.best_fragments.append(fragment)
                 if best_tail_contig not in template.best_fragments:
-                    for item in template.best_fragments:
-                        if best_tail_contig not in item:
-                            template.best_fragments.append(best_tail_contig)
+                    template.best_fragments.append(best_tail_contig)
                 # hook = best_tail_contig[len(best_tail_contig) - 3:]
                 # print(best_tail_contig,hook)
                 # hook_out = f'{froot}/hook_refactor.m8'

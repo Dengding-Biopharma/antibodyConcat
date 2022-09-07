79ee50256b544f48bd12a295290daf2433904f33
Zhijian Mei <545902915@qq.com>
2022-09-07 14:44:26 +0800

    Update concatenateBestFragments.py

--- concatenateBestFragments.py
+++ concatenateBestFragments.py
@@ -80,8 +80,12 @@ if __name__ == '__main__':
                 for i in range(len(candidate_bases)):
                     candidate_bases[i].append(candidate_letters[0])
             if num_candidates_letters > 1:
+                step = len(candidate_bases)
                 for i in range(num_candidates_letters-1):
                     candidate_bases = candidate_bases + candidate_bases
-                print(candidate_bases)
-                quit()
+                for i in range(len(candidate_letters)):
+                    candidate_letter = candidate_letters[i]
+                    for j in range(i,len(candidate_bases),step):
+                        candidate_bases[j].append(candidate_letter)
+        print(candidate_bases)
         quit()

2bf7622bb35aaec20f40f7f4bfb0f37e41b814d8
Zhijian Mei <545902915@qq.com>
2022-09-07 13:03:13 +0800

    Update concatenateBestFragments.py

--- concatenateBestFragments.py
+++ concatenateBestFragments.py
@@ -57,7 +57,8 @@ if __name__ == '__main__':
             value = candidate_fragments_dic[candidate_fragment]
             if (value[0][1] - value[0][0]) == (value[1][1] - value[1][0]):  # 长度匹配上了
                 print(value)
-                for i in range(value[0][0],value[0][1]):
+                shift = value[1][0]
+                for i in range(value[0][0]+shift,value[0][1]+shift):
                     if best_fragments[candidate_fragment][i] not in line.position[i]:
                         line.position[i].append(candidate_fragment[i])
         print(line.position)

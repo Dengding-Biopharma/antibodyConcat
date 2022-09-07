b86c16545829d3a56d9fd5356055e596b3299310
Zhijian Mei <545902915@qq.com>
2022-09-07 12:09:32 +0800

    Update concatenateBestFragments.py

--- concatenateBestFragments.py
+++ concatenateBestFragments.py
@@ -48,6 +48,6 @@ if __name__ == '__main__':
         for candidate_fragment in candidate_fragments_dic.keys():
             value = candidate_fragments_dic[candidate_fragment]
             if (value[0][1] - value[0][0]) == (value[1][1] - value[1][0]): # 长度匹配上
-                if len(best_fragments[candidate_fragment]) >= (value[0][1] - value[0][0]):
+                if len(best_fragments[candidate_fragment]) == (value[0][1] - value[0][0]):
                     print(value)
         quit()

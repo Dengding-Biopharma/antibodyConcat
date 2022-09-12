524dc4230953fa2e348db3e56501aecc08e1c744
Zhijian Mei <545902915@qq.com>
2022-09-12 20:54:25 +0800

    Update VI_concatenateBestFragments.py

--- VI_concatenateBestFragments.py
+++ VI_concatenateBestFragments.py
@@ -133,9 +133,9 @@ if __name__ == '__main__':
                     for candidate_letter in candidate_letters:
                         for candidate_base in candidate_bases_copy:
                             candidate_bases.append(candidate_base+candidate_letter)
-                        if len(candidate_bases) > 1000000:
-                            candidate_bases = sorted(candidate_bases,
-                                                     key=lambda x: findSupportReadScore(x, sequences_scores), reverse=True)[:len(candidate_bases)//2]
+                        # if len(candidate_bases) > 1000000:
+                        #     candidate_bases = sorted(candidate_bases,
+                        #                              key=lambda x: findSupportReadScore(x, sequences_scores), reverse=True)[:len(candidate_bases)//2]
             candidate_bases = sorted(candidate_bases,key=lambda x:findSupportReadScore(x,sequences_scores),reverse=True)
             print('number of candidate bases: ',len(candidate_bases))
             base = candidate_bases[0]

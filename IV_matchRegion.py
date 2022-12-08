2bdbc0243b028cdf08c85d40966fb46517d6cf28
Zhijian Mei <545902915@qq.com>
2022-12-08 15:26:32 +0800

    Update IV_matchRegion.py

--- IV_matchRegion.py
+++ IV_matchRegion.py
@@ -142,10 +142,10 @@ if __name__ == '__main__':
                     f.write(f'>{region_sequence_key}\n{region_sequence}')
             os.system(f'prerapsearch -d {template_name} -n {froot}/temp-db')
             ## contig matching
-            os.system(f'rapsearch -q {froot}/{froot}_sorted.fasta -d {froot}/temp-db -o {froot}/region_rapsearch_outputs -z 6')
+            # os.system(f'rapsearch -q {froot}/{froot}_sorted.fasta -d {froot}/temp-db -o {froot}/region_rapsearch_outputs -z 6')
             ## denovo matching
-            # os.system(
-            #     f'rapsearch -q {froot}/input_reads.fasta -d {froot}/temp-db -o {froot}/region_rapsearch_outputs -z 6')
+            os.system(
+                f'rapsearch -q {froot}/input_reads.fasta -d {froot}/temp-db -o {froot}/region_rapsearch_outputs -z 6')
             os.system(
                 f'python processRapsearchM8.py -input {froot}/region_rapsearch_outputs.m8 -output {froot}/region_rapsearch_outputs_refactor.m8')
 

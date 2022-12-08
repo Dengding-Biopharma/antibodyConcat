cbe9f709c1a322c7d6f34dfd932b838800bde257
Zhijian Mei <545902915@qq.com>
2022-12-08 13:13:56 +0800

    Update IV_matchRegion.py

--- IV_matchRegion.py
+++ IV_matchRegion.py
@@ -129,7 +129,7 @@ if __name__ == '__main__':
     best_template = open(f'{froot}/best_templates.fasta','w')
     for region in regions:
         for chain in chains:
-            find = False
+            # find = False
             region_file = f'templates/{region}_{template}_{chain}.fasta'
             region_sequence_dic = read_fasta(region_file)
             region_sequence_coverage_dic = {}
@@ -148,33 +148,36 @@ if __name__ == '__main__':
                 f'rapsearch -q {froot}/input_reads.fasta -d {froot}/temp-db -o {froot}/region_rapsearch_outputs -z 6')
             os.system(
                 f'python processRapsearchM8.py -input {froot}/region_rapsearch_outputs.m8 -output {froot}/region_rapsearch_outputs_refactor.m8')
-            try:
-                df = pd.read_csv(f'{froot}/region_rapsearch_outputs_refactor.m8', delimiter='\t', header=None)
-            except:
-                continue
+
+            df = pd.read_csv(f'{froot}/region_rapsearch_outputs_refactor.m8', delimiter='\t', header=None)
             df = df[df[2] >= 80]
             df = df.reset_index(drop=True)
-            df = df[df[1] == 'sp|31495|Heavy|Homo_sapiens']
-            print(df)
-            quit()
-            dfList = df.values
-            sequence_template_id_pair_dic = {}
-            for item in dfList:
-                template_id = item[:2][1]
-                label = item[:2][0] + '+' + template_id
-                value_list = list(item[2:])
-                sequence_template_id_pair_dic[label] = value_list
-            blank_sequence = list('0'*len(region_sequence))
-            for label in sequence_template_id_pair_dic.keys():
-                value = sequence_template_id_pair_dic[label]
-                if value[5]-value[4] != (value[7]-value[6]):
+            for index in trange(len(keys)):
+                region_sequence_key = keys[index]
+                try:
+                    sub_df = df[df[1] == region_sequence_key]
+                except:
                     continue
-                for i in range(value[6]-1,value[7]):
-                    blank_sequence[i] = '1'
-            coverage = blank_sequence.count('1')/len(blank_sequence)
-            region_sequence_coverage_dic[region_sequence_key] = coverage
-            if coverage > 0.97 or index == (len(keys) - 1):
-                find = True
+                dfList = df.values
+                print(df)
+                quit()
+                sequence_template_id_pair_dic = {}
+                for item in dfList:
+                    template_id = item[:2][1]
+                    label = item[:2][0] + '+' + template_id
+                    value_list = list(item[2:])
+                    sequence_template_id_pair_dic[label] = value_list
+                blank_sequence = list('0'*len(region_sequence))
+                for label in sequence_template_id_pair_dic.keys():
+                    value = sequence_template_id_pair_dic[label]
+                    if value[5]-value[4] != (value[7]-value[6]):
+                        continue
+                    for i in range(value[6]-1,value[7]):
+                        blank_sequence[i] = '1'
+                coverage = blank_sequence.count('1')/len(blank_sequence)
+                region_sequence_coverage_dic[region_sequence_key] = coverage
+            # if coverage > 0.97 or index == (len(keys) - 1):
+            #     find = True
             region_sequence_coverage_dic = dict(sorted(region_sequence_coverage_dic.items(), key=lambda item: item[1],reverse=True))
             best_template_id = list(region_sequence_coverage_dic.items())[0][0]
             best_coverage = list(region_sequence_coverage_dic.items())[0][1]

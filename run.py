42948cd859460c2a00d28ffbf51139063d40c5c3
Zhijian Mei <545902915@qq.com>
2023-01-03 11:54:02 +0800

    Update run.py

--- run.py
+++ run.py
@@ -6,14 +6,12 @@ def get_args():
     parser = argparse.ArgumentParser()
     # start
     parser.add_argument('-source_path',type=str,required=True)
-    parser.add_argument('-spectrum_require',type=int,default=0)
     parser.add_argument('-spectrum_path',type=str,default=None)
     parser.add_argument('-source', type=str, required=True)
     parser.add_argument('-score', type=float,default=0.8)
     parser.add_argument('-t', type=int,default=2)
     parser.add_argument('-kl', type=int,default=5)
     parser.add_argument('-ku', type=int,default=8)
-    parser.add_argument('-more', type=int, default=0)
     parser.add_argument('-template',type=str,default='homo')
     parser.add_argument('-predfull_path',type=str,required=True)
     # parser.add_argument('-rapsearch_path',type=str,required=True)
@@ -34,17 +32,13 @@ if __name__ == '__main__':
     data_path = args.source_path
     assert data_path is not None
 
-    if args.spectrum_require == 1:
-        spectrum_path = args.spectrum_path
-        if not spectrum_path:
-            print('Please provide the spectrum path as you required it to be data argument.')
-            quit(1)
+    spectrum_path = args.spectrum_path
 
     froot = f'{args.source}_{args.kl}-{args.ku}mer_{args.score}_{args.t}'
-    if args.more == 1:
-        os.system(f'python I_generateInputReads.py -source {args.source} -score {args.score} -t {args.t} -kl {args.kl} -ku {args.ku} -more {args.more} -predfull_path {predfull_path} -msSlash_path {msslash_path} -source_path {data_path} -spectrum_path {spectrum_path}')
+    if spectrum_path is not None:
+        os.system(f'python I_generateInputReads.py -source {args.source} -score {args.score} -t {args.t} -kl {args.kl} -ku {args.ku} -more 1 -predfull_path {predfull_path} -msSlash_path {msslash_path} -source_path {data_path} -spectrum_path {spectrum_path}')
     else:
-        os.system(f'python I_generateInputReads.py -source {args.source} -score {args.score} -t {args.t} -kl {args.kl} -ku {args.ku} -more {args.more} -predfull_path {predfull_path} -msSlash_path {msslash_path} -source_path {data_path}')
+        os.system(f'python I_generateInputReads.py -source {args.source} -score {args.score} -t {args.t} -kl {args.kl} -ku {args.ku} -more 0 -predfull_path {predfull_path} -msSlash_path {msslash_path} -source_path {data_path}')
     os.system(f'python II_assembleFromReads.py -froot {froot}')
     os.system(f'python III_sortOutputs.py -froot {froot}')
     os.system(f'python IV_matchRegion.py -froot {froot} -template {args.template}')

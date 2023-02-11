d101d0d5c338bbf0ef95bed96cb979da9db9138b
Zhijian Mei <545902915@qq.com>
2023-02-11 21:00:52 +0800

    Update IV_matchRegion_multi.py

--- IV_matchRegion_multi.py
+++ IV_matchRegion_multi.py
@@ -150,57 +150,27 @@ if __name__ == '__main__':
             continue
         if sub_df.empty:
             continue
-        print(sub_df)
-        quit()
-
-    for candidates_template in candidates_templates:
-
-        keys = list(region_sequence_dic.keys())
-        template_name = f'{froot}/region_temp.fasta'
-        with open(template_name,'w') as f:
-            for index in trange(len(keys)):
-                region_sequence_key = keys[index]
-                region_sequence = region_sequence_dic[region_sequence_key]
-                f.write(f'>{region_sequence_key}\n{region_sequence}')
-        os.system(f'prerapsearch -d {template_name} -n {froot}/temp-db')
-        ## contig matching
-        os.system(f'rapsearch -q {froot}/contigs_sorted.fasta -d {froot}/temp-db -o {froot}/region_rapsearch_outputs -z 6')
-        ## denovo matching
-        # os.system(
-        #     f'rapsearch -q {froot}/input_reads.fasta -d {froot}/temp-db -o {froot}/region_rapsearch_outputs -z 6')
-        os.system(
-            f'python processRapsearchM8.py -input {froot}/region_rapsearch_outputs.m8 -output {froot}/region_rapsearch_outputs_refactor.m8')
-
-        df = pd.read_csv(f'{froot}/region_rapsearch_outputs_refactor.m8', delimiter='\t', header=None)
-        df = df[df[2] >= 80]
-        df = df.reset_index(drop=True)
-        for index in trange(len(keys)):
-            region_sequence_key = keys[index]
-            region_sequence = region_sequence_dic[region_sequence_key]
-            try:
-                sub_df = df[df[1] == region_sequence_key]
-            except:
+        dfList = sub_df.values
+        sequence_template_id_pair_dic = {}
+        for item in dfList:
+            template_id = item[:2][1]
+            label = item[:2][0] + '+' + template_id
+            value_list = list(item[2:])
+            sequence_template_id_pair_dic[label] = value_list
+        for label in sequence_template_id_pair_dic.keys():
+            value = sequence_template_id_pair_dic[label]
+            if value[5] - value[4] != (value[7] - value[6]):
                 continue
-            dfList = sub_df.values
-            sequence_template_id_pair_dic = {}
-            for item in dfList:
-                template_id = item[:2][1]
-                label = item[:2][0] + '+' + template_id
-                value_list = list(item[2:])
-                sequence_template_id_pair_dic[label] = value_list
-            blank_sequence = list('0'*len(region_sequence))
-            for label in sequence_template_id_pair_dic.keys():
-                value = sequence_template_id_pair_dic[label]
-                if value[5]-value[4] != (value[7]-value[6]):
-                    continue
-                for i in range(value[6]-1,value[7]):
-                    blank_sequence[i] = '1'
+            current_template = Templates[keys[i]]
+            for j in range(value[6] - 1, value[7]):
+                if current_template.sequence[j] == 'F':
+                    current_template.sequence[j] = '1'
 
-            coverage = blank_sequence.count('1')/len(blank_sequence)
-            region_sequence_coverage_dic[region_sequence_key] = coverage
-            # if coverage > 0.97 or index == (len(keys) - 1):
-            #     find = True
-            region_sequence_coverage_dic = dict(sorted(region_sequence_coverage_dic.items(), key=lambda item: item[1],reverse=True))
-            best_template_id = list(region_sequence_coverage_dic.items())[0][0]
-            best_coverage = list(region_sequence_coverage_dic.items())[0][1]
-            best_template.write(f'>{best_template_id}_{region}_{best_coverage}\n{region_sequence_dic[best_template_id]}\n')
+        print(current_template.sequence)
+        print(current_template.getCoverage())
+        quit()
+            #
+            # region_sequence_coverage_dic = dict(sorted(region_sequence_coverage_dic.items(), key=lambda item: item[1],reverse=True))
+            # best_template_id = list(region_sequence_coverage_dic.items())[0][0]
+            # best_coverage = list(region_sequence_coverage_dic.items())[0][1]
+            # best_template.write(f'>{best_template_id}_{region}_{best_coverage}\n{region_sequence_dic[best_template_id]}\n')
